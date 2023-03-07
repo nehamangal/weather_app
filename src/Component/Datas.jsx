57e7747785481c0b4ad618846757ff631c78737c
nehamangal <76638350+nehamangal@users.noreply.github.com>
2023-03-07 15:28:07 +0530

    Update Datas.jsx

--- src/Component/Datas.jsx
+++ src/Component/Datas.jsx
@@ -1,11 +1,16 @@
 import '../App.scss'
 const Data=({tempmin,City_Name,tempmax})=>{
- 
+  const date = new Date();
+  const showTime = date.getHours() 
+      + ':' + date.getMinutes() 
+      + ":" + date.getSeconds();
+      const showDate=date.getDate()+'/'+date.getMonth()+'/'+date.getFullYear();
 if(tempmax){
   return(
   <div className="getdata">
      <h1>{City_Name}</h1>
      <p>{`Min_temp :${tempmin}`}&deg; C || {`Max_temp:${tempmax}`}&deg; C</p>
+     <p>{showDate} || {showTime}</p>
   </div>
   )
 }
