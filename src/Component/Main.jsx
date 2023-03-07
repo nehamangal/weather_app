07f7deb468617b4893f1bd5b98dea801029e072b
nehamangal <76638350+nehamangal@users.noreply.github.com>
2023-03-07 15:22:22 +0530

    Update Main.jsx

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { useState } from "react"
 import Data from "./Datas";
-import {BsFillSunFill} from 'react-icons/bs'
 import axios from 'axios'
 import weather from '../weather.png'
 // import {TiWeatherCloudy} from 'react-icons/ti'
@@ -10,7 +9,6 @@ import img from '../cloud.jpg'
 const round = require('math-round')
 
 const api_key = "139a7d420f1aefcfdf4e8788de932a75"
-const rep = 1;
 const Main=()=>{
    const [initialcity,setcity]=useState("");
    const [initial_local,setlocation]=useState("");
@@ -23,13 +21,12 @@ const Main=()=>{
       const {data} =  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${initialcity}&appid=${api_key}`);
       // setemps(data.main.temp);
       var temp_minimum = data.main.temp_min;
-      temp_minimum=((temp_minimum-32)*5)/9;
+      temp_minimum = temp_minimum-273.15
       temp_minimum=round(temp_minimum);
-      // temp_minimum = {Math.round({temp_minimum})}%;
       
 
       let temp_maximum=data.main.temp_max;
-      temp_maximum=((temp_maximum-32)*5)/9;
+      temp_maximum = temp_maximum-273.15
       temp_maximum=round(temp_maximum);
    
 
@@ -48,6 +45,7 @@ const Main=()=>{
 
     <div className="Main_part">
     <form onSubmit={submission}>
+      <h1>Weather App</h1>
         <input type="text" value={initialcity} placeholder="Enter Your City Name" onChange={(e)=>{
             setcity(e.target.value)
            
