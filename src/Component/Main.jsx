19129d4e513bd388b28cdaaf4c60a406c7d13d2b
nehamangal <76638350+nehamangal@users.noreply.github.com>
2023-04-07 00:24:40 +0530

    Update Main.jsx

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -3,10 +3,14 @@ import { useState } from "react"
 import Data from "./Datas";
 import axios from 'axios'
 import weather from '../weather.png'
+import { Country, State}  from 'country-state-city';
 // import {TiWeatherCloudy} from 'react-icons/ti'
 // import { useEffect } from "react";
 import img from '../cloud.jpg'
-const round = require('math-round')
+const round = require('math-round');
+const arr=Country.getAllCountries();
+const arr2=State.getAllStates();
+
 
 const api_key = "139a7d420f1aefcfdf4e8788de932a75"
 const Main=()=>{
@@ -14,12 +18,14 @@ const Main=()=>{
    const [initial_local,setlocation]=useState("");
    const [mintempe,setmintemps]=useState()
    const [maxtempe,setmaxtemps]=useState()
+   const [ispresent,setpresent]=useState("false");
 
   
    const submission=async(e)=>{
       e.preventDefault();
+     try{
       const {data} =  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${initialcity}&appid=${api_key}`);
-      // setemps(data.main.temp);
+   //   console.log(data.name);
       var temp_minimum = data.main.temp_min;
       temp_minimum = temp_minimum-273.15
       temp_minimum=round(temp_minimum);
@@ -29,12 +35,25 @@ const Main=()=>{
       temp_maximum = temp_maximum-273.15
       temp_maximum=round(temp_maximum);
    
-
+     console.log(temp_maximum);
+     console.log(temp_minimum);
       setmaxtemps(temp_minimum)
       setmintemps(temp_maximum)
       setlocation(data.name);
-      setcity("")
+      setcity("");  
    }
+      catch(err){
+        {
+         alert("Please write the correct city/country name")
+        }
+        setcity("");
+   }
+      
+     
+      
+   }
+   
+  
 
   return(
     <>
@@ -51,17 +70,17 @@ const Main=()=>{
            
         }}/>
         <br />
-        <button >Submit</button>
+        <button>Submit</button>
      </form>
+    
+     
      <div className='datas'>
       <img style={{margin:"0.5rem 3rem" ,fontSize:"-5px" ,height:"5rem"}} src={weather} alt="" />
   </div>
      </div>
-     
-   
     
-    <Data tempmin={mintempe} City_Name={initial_local} tempmax = {maxtempe} />
-   
+     <Data tempmin={mintempe} City_Name={initial_local} tempmax = {maxtempe}/>
+     
     </>
   )
 
